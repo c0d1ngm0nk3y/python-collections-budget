5b75b44425920e705b11b9bdf4daa2ee9f411359
Ralf Pannemans <ralf.pannemans@sap.com>
2022-03-24 16:50:22 +0100

    Module 5

--- budget/Expense.py
+++ budget/Expense.py
@@ -1,42 +1,47 @@
 import csv
 from datetime import datetime
 
-class Expense():
+
+class Expense:
     def __init__(self, date_str, vendor, category, amount):
-        self.date_time = datetime.strptime(date_str, '%m/%d/%Y %H:%M:%S')
+        self.date_time = datetime.strptime(date_str, "%m/%d/%Y %H:%M:%S")
         self.vendor = vendor
         self.category = category
         self.amount = amount
 
 
-class Expenses():
+class Expenses:
     def __init__(self):
         self.list = []
         self.sum = 0
 
     # Read in the December spending data, row[2] is the $$, and need to format $$
-    def read_expenses(self,filename):
-        with open(filename, newline='') as csvfile:
-            csvreader = csv.reader(csvfile, delimiter=',')
+    def read_expenses(self, filename):
+        with open(filename, newline="") as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=",")
             for row in csvreader:
-                if '-' not in row[3]:
+                if "-" not in row[3]:
                     continue
-                amount = float((row[3][2:]).replace(',',''))
-                self.list.append(Expense(row[0],row[1], row[2], amount))
+                amount = float((row[3][2:]).replace(",", ""))
+                self.list.append(Expense(row[0], row[1], row[2], amount))
                 self.sum += amount
 
+    def categorize_set_comprehension(self):
+        necessary_expenses = [x for x in self.list if x.category in ["Phone", "Auto and Gas", "Classes", "Utilities", "Mortgage"]]
+        food_expenses = [x for x in self.list if x.category in ["Groceries", "Eating Out"]]
+        unnecessary_expenses = [x for x in self.list if x not in necessary_expenses and x not in food_expenses]
+        return [set(necessary_expenses), set(food_expenses), set(unnecessary_expenses)]
+
     def categorize_for_loop(self):
-            necessary_expenses = set()
-            food_expenses = set()
-            unnecessary_expenses = set()
-            for i in self.list:
-                if (i.category == 'Phone'    or i.category == 'Auto and Gas' or 
-                    i.category == 'Classes'  or i.category == 'Utilities' or 
-                    i.category == 'Mortgage'): 
-                    necessary_expenses.add(i)
-                elif(i.category == 'Groceries' or i.category == 'Eating Out'):
-                    food_expenses.add(i)
-                else:
-                    unnecessary_expenses.add(i)
-            
-            return [necessary_expenses, food_expenses, unnecessary_expenses]
+        necessary_expenses = set()
+        food_expenses = set()
+        unnecessary_expenses = set()
+        for i in self.list:
+            if i.category == "Phone" or i.category == "Auto and Gas" or i.category == "Classes" or i.category == "Utilities" or i.category == "Mortgage":
+                necessary_expenses.add(i)
+            elif i.category == "Groceries" or i.category == "Eating Out":
+                food_expenses.add(i)
+            else:
+                unnecessary_expenses.add(i)
+
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,10 +1,39 @@
 from . import Expense
-import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt
+
+
+def check_sets(x, y):
+    print()
+    print(len(x), len(y))
+    if x.issubset(y):
+        print("x is subset of y")
+    if y.issubset(x):
+        print("y is subset of x")
+    for e1, e2 in zip(x, y):
+        if e1.amount != e2.amount:
+            print(e1.amount, e2.amount)
+
 
 def main():
     expenses = Expense.Expenses()
-    expenses.read_expenses('data/spending_data.csv')
+    expenses.read_expenses("data/spending_data.csv")
     divided_for_loop = expenses.categorize_for_loop()
+    divided_set_comp = expenses.categorize_set_comprehension()
+
+    if divided_for_loop != divided_set_comp:
+        print("Sets are NOT equal by == test")
+
+    [a, b, c] = divided_for_loop
+    [d, e, f] = divided_set_comp
+
+    check_sets(a, d)
+    check_sets(b, e)
+    check_sets(c, f)
+
+    for x, y in zip(divided_for_loop, divided_set_comp):
+        if not (x.issubset(y) and x.issubset(y)):
+            print("Sets are NOT equal by subset test")
 
 
 if __name__ == "__main__":
